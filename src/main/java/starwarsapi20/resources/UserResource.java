6e5539f9865558d270bfb96d2f3f5d35f25e55fb
MIN00S <rennan771@gmail.com>
2024-02-28 14:34:00 -0300

    Service layer, component registration

--- src/main/java/starwarsapi20/resources/UserResource.java
+++ src/main/java/starwarsapi20/resources/UserResource.java
@@ -1,17 +1,31 @@
 package starwarsapi20.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import starwarsapi20.entities.User;
+import starwarsapi20.services.UserService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Jose", "18", "Masculino", "verdadeiro");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
